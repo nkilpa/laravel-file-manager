90131e120d5bdf510a2903b6b520bcc9fded5a6a
nikitakilpa <nikitakilpa@gmail.com>
2022-03-23 17:12:07 +0300

    багфикс

--- src/Exports/ProductExport.php
+++ src/Exports/ProductExport.php
@@ -3,10 +3,30 @@
 namespace nikitakilpa\FileManager\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use nikitakilpa\FileManager\Models\Product;
 
-class ProductExport implements FromCollection
+class ProductExport implements FromCollection, WithHeadings
 {
+    public function headings(): array
+    {
+        return [
+            'id',
+            'Номер',
+            'Наименование',
+            'Производитель',
+            'Бренд',
+            'Тип',
+            'Категория',
+            'Описание',
+            'Количество',
+            'Цена',
+            'КемДобавлено',
+            'Создано',
+            'Обновлено',
+        ];
+    }
+
     public function collection()
     {
         return Product::all();
